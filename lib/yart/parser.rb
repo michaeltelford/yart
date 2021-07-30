645f261d8fe0195c7edfb18951a7958fdba5778b
Michael Telford <michael.telford@live.com>
2021-07-30 14:53:35 +0100

    Fixes and refactor

--- lib/yart/parser.rb
+++ lib/yart/parser.rb
@@ -1,61 +1,72 @@
 # frozen_string_literal: true
 
 module YART::Parser
+  # Parses a block of Ruby, generating HTML.
   def parse(&block)
-    raise "Must pass a block to render HTML" unless block_given?
+    raise "Must pass a block to parse" unless block_given?
 
     @@yart_buffer = []
     instance_eval(&block)
     @@yart_buffer.join
   end
 
+  # Allows elements to be called and rendered.
   def method_missing(m, *args, &block)
     attributes = args.fetch(0, {})
-    raise "The element attributes must be a Hash" unless attributes.is_a?(Hash)
 
     render(m, attributes, &block)
   end
 
-  private
-
-  # Renders an element with the raw name (doesn't have to be lowercase).
+  # Renders an element with the raw name (case insensitive).
   def element(name, **attributes, &block)
     render(name, attributes, &block)
   end
 
-  # Sets the innerText of the element being rendered.
-  def text(&block)
-    str = block.call
-    raise "text's block must return a String" unless str.is_a?(String)
-
-    str
+  # Overrides Ruby's p method to render the element instead of printing.
+  def p(**attributes, &block)
+    render("p", attributes, &block)
   end
 
-  # Override Ruby's p method to render the element instead of printing.
-  def p(**attributes, &block)
-    render("p", **attributes, &block)
+  # Sets the innerText of the element being rendered.
+  def text(str = nil, &block)
+    raise "Must pass a String param or a block returning a String" unless str || block_given?
+
+    str ||= block.call
+    buffer(str)
+  end
+
+  private
+
+  def buffer(str)
+    @@yart_buffer << str if str.is_a?(String)
   end
 
   def render(element, attributes, &block)
-    @@yart_buffer << build_opening_tag(element, attributes)
-    @@yart_buffer << instance_eval(&block) if block_given?
-    @@yart_buffer << "</#{element}>"
+    raise "Must pass attributes as a Hash" unless attributes.is_a?(Hash)
+
+    buffer(build_opening_tag(element, attributes))
+    buffer(instance_eval(&block)) if block_given?
+    buffer(build_closing_tag(element))
   end
 
   def build_opening_tag(element, attributes)
     attributes = convert_attributes(attributes)
-    html_attributes = attributes.
-      map {|k, v| "#{k}='#{v}'" }.
-      join(" ")
+    html_attributes = attributes
+      .map {|k, v| "#{k}='#{v}'" }
+      .join(" ")
     separator = html_attributes.empty? ? "" : " "
 
     "<#{element}#{separator}#{html_attributes}>"
   end
 
+  def build_closing_tag(element)
+    "</#{element}>"
+  end
+
   def convert_attributes(attributes)
     attributes.map do |k, v|
       k = kebab_case(k)
-      v = v.respond_to?(:each) ?
+      v = v.respond_to?(:map) ?
         v.map { |v2| convert_attribute_value(v2) }.join(" ") :
         convert_attribute_value(v)
 
